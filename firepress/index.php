9af95988c49f3f47d1d9c208f2d8aab2280cde72
Michael Auer <michael@michael-auer.co>
2016-02-29 15:18:37 -0500

    Generate html table based on static url

--- firepress/index.php
+++ firepress/index.php
@@ -18,14 +18,16 @@ function fp_init() {
 function fp_renderPlugin() {
   echo "<h1>Firepress</h1>";
   echo "<p>A plugin for viewing your Firebase plugins in Wordpress</p>";
+  echo "<table id=\"fp_table\"></table>";
 }
 
-function fp_register_scripts() {
+function fp_register_files() {
   wp_enqueue_script('fp_firebase_js', 'https://cdn.firebase.com/js/client/2.4.1/firebase.js');
   wp_enqueue_script('fp_app_js', plugins_url('/js/app.js', __FILE__));
+  wp_enqueue_style('fp_style_css', plugins_url('/css/style.css', __FILE__));
 }
 
-add_action('admin_menu', 'fp_register_scripts');
+add_action('admin_menu', 'fp_register_files');
 add_action('admin_menu', 'fp_init');
 
 ?>
--- firepress/js/app.js
+++ firepress/js/app.js
@@ -1,5 +1,37 @@
-var firebase = new Firebase("https://signaturerepo.firebaseio.com/");
+var firebase = new Firebase('https://signaturerepo.firebaseio.com/leads');
 
-firebase.child('leads').on('value', function(snapshot) {
-  console.log(snapshot.val());
-});
+firebase.on('value', function(snapshot) {
+  var fp_data = snapshot.val()
+  var fp_content_header = ''
+  var fp_content = ''
+  var fp_data_lead;
+
+  // Loop through the leads object
+  Object.keys(fp_data).forEach(function(k,i) {
+
+    // Start the row
+    fp_content += '<tr>'
+
+    // Cache the lead object
+    fp_data_lead = fp_data[k]
+
+    // Cache the index for checking if this is the first element
+    fp_data_index = i
+
+    // Loop through the lead's properties
+    Object.keys(fp_data_lead).forEach(function(k,i) {
+      // We need the key to populate our header (only the first time)
+      if (fp_data_index === 0) {
+        fp_content_header += '<th>' + k + '</th>'
+      }
+      fp_content += '<td>' + fp_data_lead[k] + '</td>'
+    })
+    fp_content += '</tr>'
+  })
+
+  var fp_table = document.getElementById('fp_table')
+
+  fp_table.innerHTML = fp_content
+  fp_table.insertAdjacentHTML('afterbegin', fp_content_header)
+
+})
